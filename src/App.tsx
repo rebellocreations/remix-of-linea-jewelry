97415317b108b9cfb69ef3d4925813d130f9deb9
Tushar <tushar@Tushars-MacBook-Pro.local>
2026-03-15 13:16:01 +0530

    Add Vercel Analytics for page view tracking

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import ScrollToTop from "./components/ScrollToTop";
+import { Analytics } from "@vercel/analytics/react";
 
 const Index = lazy(() => import("./pages/Index"));
 import Category from "./pages/Category";
@@ -72,6 +73,7 @@ const App = () => (
 
         <ScrollToTop />
         <AnimatedRoutes />
+        <Analytics />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
